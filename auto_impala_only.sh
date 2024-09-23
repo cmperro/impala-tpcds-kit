2f2ca56c2c918d1ab556a9f1eb6e2a41675f58a4
Christopher Perro <cmperro@gmail.com>
2024-09-23 09:50:03 -0400

    Impala only and increase loop

--- auto_impala_only.sh
+++ auto_impala_only.sh
@@ -18,11 +18,11 @@ do
    if [ $selected_host_number == 1 ]
    then
       echo "Hive"
-      cd /home/cperro/hive-testbench; echo $password | kinit $selected_user; bash run_some_hive_queries.sh $hive_database
+      #cd /home/cperro/hive-testbench; echo $password | kinit $selected_user; bash run_some_hive_queries.sh $hive_database
    elif [ $selected_host_number == 2 ] 
    then
       echo "Spark"
-      cd /home/cperro/hive-testbench; echo $password | kinit $selected_user; spark3-submit run_some_spark_queries.py $query_num $spark_database
+      #cd /home/cperro/hive-testbench; echo $password | kinit $selected_user; spark3-submit run_some_spark_queries.py $query_num $spark_database
    else
       echo "Impala"
       cd /home/cperro/impala-tpcds-kit; bash run_some_impala_queries.sh $password $dm_knox_host $dm_impala_path $impala_database $selected_user
