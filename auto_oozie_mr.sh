fb91c73020493c147516749a682e2b462d74167d
Christopher Perro <cmperro@gmail.com>
2024-09-23 10:28:48 -0400

    update scale on MR job

--- auto_oozie_mr.sh
+++ auto_oozie_mr.sh
@@ -12,7 +12,7 @@ do
    if [[ $(( $i % 3 )) == 0 ]]
    then
       echo "Running Map Reduce Job"
-      cd /home/cperro/impala-tpcds-kit/tpcds-gen; echo $password | kinit $selected_user; hadoop jar target/tpcds-gen-1.0-SNAPSHOT.jar -d s3a://${bucket}/data/tpcds_300_text -p 20 -s 300; hdfs dfs -rm -r -skipTrash s3a://${bucket}/data/tpcds_300_text
+      cd /home/cperro/impala-tpcds-kit/tpcds-gen; echo $password | kinit $selected_user; hadoop jar target/tpcds-gen-1.0-SNAPSHOT.jar -d s3a://${bucket}/data/tpcds_300_text -p 30 -s 300; hdfs dfs -rm -r -skipTrash s3a://${bucket}/data/tpcds_300_text
       echo "Running Oozie Job"
       cd /home/cperro/impala-tpcds-kit/oozie-wf; echo $password | kinit $selected_user; oozie job -oozie ${oozie_knox_endpoint} -auth BASIC -username $selected_user -password ${password} -config job.properties -run
    else
